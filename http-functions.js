79af31ddd5133c06ee6d7b9e1081eade9ade63d9
keano voelkl <keanov@gmail.com>
2017-07-03 16:20:22 +0000

    module tests passed

---
+++ http-functions.js
@@ -0,0 +1,21 @@
+module.exports = function getHTML (options, callback) {
+
+  var https = require('https');
+
+  https.get(options, function (response) {
+
+    var buffer = '';
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buffer += data;
+    });
+
+    response.on('end', function() {
+      callback(buffer);
+    });
+
+  });
+
+};
